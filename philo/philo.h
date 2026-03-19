8566068971a9d5ee13653484c1dec02ec9f2c5e1
derfl8 <alfred.begou@gmail.com>
2026-03-19 15:51:44 +0100

    Good struct ??

--- philo/philo.h
+++ philo/philo.h
@@ -6,11 +6,13 @@
 /*   By: abegou <abegou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/14 16:00:02 by abegou            #+#    #+#             */
-/*   Updated: 2026/03/16 15:06:45 by abegou           ###   ########.fr       */
+/*   Updated: 2026/03/19 15:50:30 by abegou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #define PHILO_H
+
+// libs
 #include <limits.h>
 #include <pthread.h>
 #include <stdbool.h>
@@ -28,3 +30,26 @@ size_t		ft_strlenin(const char *s);
 void		*ft_free(char **array);
 char		**ft_split(char const *s, char c);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
+
+// structs
+typedef struct s_philo	{
+	int			    pid;
+	long long		last_eat;
+    int             n_eat;
+	pthread_mutex_t	*lfork;
+	pthread_mutex_t	*rfork;
+	t_data			*data;
+	pthread_t		thread;
+}					t_philo;
+
+typedef struct s_data  {
+    int				nb_philos;
+    int				time_to_die;
+    int				time_to_eat;
+    int				time_to_sleep;
+	int				must_eat;
+	long long		start;
+	int				end;
+	pthread_mutex_t	*forks;
+	t_philo			*philo;
+}					t_data;
